ec1379ba7aebdff631ea790bdbf9a855f890732d
dimaswisodewo <v00029606@danamon.co.id>
2026-07-15 09:51:34 +0700

    Initial Commit

---
+++ ArchitectureReference/ArchitectureReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureReferenceApp.swift
+//  ArchitectureReference
+//
+//  Created by Meynabel Dimas Wisodewo on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
